c9a199a0bae49cfcef18fcb19d483eafe776eb6a
Chris7val <chris7valdez2001@gmail.com>
2022-07-13 15:27:12 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+# conditional Test
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+#
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+# simple if statement
+# alien_color = 'red'
+#
+# if alien_color == 'green':
+#     print("You earned 5 points fo shooting the alien")
+# elif alien_color == 'yellow':
+#     print("You earned 10 points")
+# elif alien_color == 'red':
+#     print("You earned 15 points")
+
+# stage of life if statement
+# age = 55
+# print(age)
+# if age < 2:
+#     print("The Person is a baby!")
+# elif age < 4:
+#     print("The Person is a toddler!")
+# elif age < 13:
+#     print("The Person is a Kid")
+# elif age < 20:
+#     print("The Person is a teenager")
+# elif age < 65:
+#     print("The Person is a adult")
+# elif age > 65:
+#     print("The Person is a elder")
+
+# favorite fruit if statements with list
+# favorite_fruit = ['apple', 'banana', 'grape']
+# print(favorite_fruit)
+# for fruits in favorite_fruit:
+#     if fruits == 'apple':
+#         print(f"\nYou really like {fruits.title()}")
+#     elif fruits == 'banana':
+#         print(f"\nYou really love {fruits.title()}")
+#     elif fruits == 'grape':
+#         print(f"\nYou really love {fruits.title()}")
+#     elif fruits == 'blueberry':
+#         print(f"\nYou really love {fruits.title()}")
+#     elif fruits == 'strawberry':
+#         print(f"\nYou really love {fruits.title()}")
+
+# Loop list if statements
+# users = ['admin', 'chris', 'ash', 'mike', 'luke']
+# if users:  # checking to see if list is empty
+#     for user in users:
+#         if user == 'admin':
+#             print("Hello admin, would you like to see a status report?")
+#         else:
+#             print(f"Hello {user}, thank you for logging in again")
+# else:
+#     print("We need to find some users!")
+
+# Checking usernames using list for loops and if statements
+# current_users = ['alien head', 'brainy', 'chris', 'ash', 'blake']
+# new_users = ['chris', 'ash', 'fire', 'time', 'earth']
+#
+# for new_user in new_users:
+#     if new_user.casefold() in current_users:
+#         print(f"The username {new_user} is in use please choose a new username.")
+#     else:
+#         print(f"The username {new_user} is available")
+#
+
+# loop list and if else
+# ordinal_num = list(range(1, 10))
+# for num in ordinal_num:
+#     if num == 1:
+#         print(f"{num}st")
+#     elif num == 2:
+#         print(f"{num}nd")
+#     elif num == 3:
+#         print(f"{num}rd")
+#     else:
+#         print(f"{num}th")
+
